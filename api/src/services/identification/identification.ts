0b0516d708e100928f645b5d5c2aa353148fcdb8
orta <git@orta.io>
2022-07-09 10:23:35 +0100

    Comments

--- api/src/services/identification/identification.ts
+++ api/src/services/identification/identification.ts
@@ -4,12 +4,19 @@ const noop = () => {
   throw new Error('This resolver does not exist:')
 }
 
+// Whenever we create a User object, the id is a generated UUID
+// and then we append :user to the end of the id. This is then
+// understood by the node resolver to go to the 'get' for a user.
+
 const nodeTypes = {
   ':user': {
     type: 'User',
     get: user,
     delete: deleteUser,
   },
+
+  // This would be a longer list of all the types in your codebase
+  // which conform to the Node interface.
 }
 
 const keys = Object.keys(nodeTypes)
